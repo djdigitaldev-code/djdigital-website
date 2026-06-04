50f338e6a48d62f3552abd2c710c919a975c2f7a
djdigitaldev-code <djdigitaldev@gmail.com>
2026-06-04 15:41:36 +0200

    fix home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,36 +1,26 @@
 export default function Home() {
   return (
-    <section>
-      <h1>Home</h1>
-    </section>
+    <div>
+
+      <section className="hero">
+
+        <h1>
+          DJ DIGITAL
+        </h1>
+
+        <p>
+          Maatwerk websites • Hosting • SEO
+        </p>
+
+        <a
+          href="/tarieven"
+          className="button"
+        >
+          Bekijk tarieven
+        </a>
+
+      </section>
+
+    </div>
   );
-}
-
-<div>
-
-<section className="hero">
-
-<h1>
-DJ DIGITAL
-</h1>
-
-<p>
-Maatwerk websites • Hosting • SEO
-</p>
-
-<a
-href="/tarieven"
-className="button"
->
-
-Bekijk tarieven
-
-</a>
-
-</section>
-
-</div>
-
-)
-
 }
