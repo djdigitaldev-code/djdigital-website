d280cd49c2dde2b88f002b8bcca00613fd950d97
djdigitaldev-code <djdigitaldev@gmail.com>
2026-07-23 13:29:32 +0200

    page

--- app/page.jsx
+++ app/page.jsx
@@ -1,70 +1,7 @@
 "use client";
 
-import { useState } from "react";
 import Link from "next/link";
-
-function Navbar() {
-  const [open, setOpen] = useState(false);
-
-  return (
-    <nav className="navbar">
-
-      <div className="nav-container">
-
-        <Link
-          href="/"
-          className="nav-logo"
-        >
-          <img
-            src="/logo.png"
-            alt="DJ Digital"
-            className="logo"
-          />
-        </Link>
-
-        <div className={`nav-links ${open ? "open" : ""}`}>
-
-          <Link href="/">
-            Home
-          </Link>
-
-          <Link href="/diensten">
-            Diensten
-          </Link>
-
-          <Link href="/tarieven">
-            Tarieven
-          </Link>
-
-          <Link href="/over-ons">
-            Over ons
-          </Link>
-
-          <Link href="/contact">
-            Contact
-          </Link>
-
-          <Link
-            href="/contact"
-            className="nav-cta"
-          >
-            Gratis gesprek
-          </Link>
-
-        </div>
-
-        <button
-          className="hamburger"
-          onClick={() => setOpen(!open)}
-        >
-          ☰
-        </button>
-
-      </div>
-
-    </nav>
-  );
-}
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
