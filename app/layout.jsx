f5da9ddde3e6d7c867a1e1ecad88886c4065aa94
djdigitaldev-code <djdigitaldev@gmail.com>
2026-07-23 13:38:44 +0200

    layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,31 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="nl">
       <body>
-
-        <nav className="navbar">
-
-          <a href="/" className="logo-wrap">
-            <img
-              src="/logo.png"
-              alt="DJ Digital"
-              className="logo"
-            />
-          </a>
-
-          <div className="nav-links">
-            <a href="/">Home</a>
-            <a href="/diensten">Diensten</a>
-            <a href="/tarieven">Tarieven</a>
-            <a href="/over-ons">Over</a>
-            <a href="/contact">Contact</a>
-          </div>
-
-        </nav>
-
-        <main>
-          {children}
-        </main>
-
+        <main>{children}</main>
       </body>
     </html>
   );
